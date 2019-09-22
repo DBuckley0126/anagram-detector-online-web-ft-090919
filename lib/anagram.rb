2c31ddb7c9101a1000cd836a9130cd1e6fa0f2d1
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-22 15:38:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+def initialize (word)
+  @word = word
+end
+
+def match (array)
+  var = @word.split(//)
+  array.each do |anagram|
+    anagram == var
+  end
+end
+
+
+
+end
